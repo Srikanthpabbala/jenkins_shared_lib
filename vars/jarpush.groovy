2549f8ada44abc14f2d2fd9c9daf633279b515ba
Srikanthpabbala <129891194+Srikanthpabbala@users.noreply.github.com>
2023-10-11 02:10:44 +0530

    Update jarpush.groovy

--- vars/jarpush.groovy
+++ vars/jarpush.groovy
@@ -1,30 +1,16 @@
-import requests
-
-# JFrog Artifactory details
-artifactory_url = "http://35.171.28.240:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
-username = "admin"
-password = "Password1"
-repo_key = "example-repo-local"  # The target repository key in Artifactory
-
-# Path to the JAR file
-jar_file_path = "kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
-
-# Construct the upload URL
-upload_url = f"{artifactory_url}/{repo_key}/{jar_file_path}"
-
-# Set up the authentication
-auth = (username, password)
-
-# Upload the JAR file
-try:
-    with open(jar_file_path, 'rb') as jar_file:
-        response = requests.put(upload_url, data=jar_file, auth=auth)
-
-    if response.status_code == 201:
-        print("Upload successful!")
-    else:
-        print(f"Upload failed with status code: {response.status_code}")
-        print(response.text)
-
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+stage('Jar push JFROG : jfrog'){
+ when{ expression { params.action == 'create' } }
+ steps{
+ script{
+ }
+ }
+ }
+ def artifactoryUrl = 'http://35.171.28.240:8081/artifactory'
+ def artifactoryRepo = 'example-repo-local'
+ def jarFileName = 'kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+ def targetPath = "${artifactoryRepo}/mamta/"
+ sh """
+ cd /var/lib/jenkins/workspace/jfrog-demo/target/
+ chmod +x${jarFileName}
+ curl-X PUT-u admin:Password1-T ${jarFileName} ${artifactoryUrl}/${targetPath}
+ """
