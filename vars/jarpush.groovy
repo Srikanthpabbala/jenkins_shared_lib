6b822f060922bb5c32d690700185b6c250757ae6
Srikanthpabbala <129891194+Srikanthpabbala@users.noreply.github.com>
2023-10-11 02:41:56 +0530

    Update jarpush.groovy

--- vars/jarpush.groovy
+++ vars/jarpush.groovy
@@ -1,3 +1,9 @@
 def call(){
     sh'./jfrog.py'
+
+    sh """
+ cd /var/lib/jenkins/workspace/jfrog-demo/target/
+ chmod +x${jarFileName}
+ curl-X PUT-u admin:Password1 -T ${jarFileName} ${artifactoryUrl}/${targetPath}
+ """  
 }
