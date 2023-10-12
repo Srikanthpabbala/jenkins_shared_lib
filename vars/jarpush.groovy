7c63ecae9ec3ef8cfa488ae7e47d68cbc2b3370c
Srikanthpabbala <129891194+Srikanthpabbala@users.noreply.github.com>
2023-10-12 19:08:34 +0530

    Delete vars/jarpush.groovy

--- vars/jarpush.groovy
+++
@@ -1,9 +0,0 @@
-def call(){
-    sh'./jfrog.py'
-
-    sh """
- cd /var/lib/jenkins/workspace/TestCI/target/
- chmod +x${jarFileName}
- curl-X PUT-u admin:Password1 -T ${jarFileName} ${artifactoryUrl}/${targetPath}
- """  
-}
